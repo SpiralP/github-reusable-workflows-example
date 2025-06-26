8ca9b4a14e8923105a4a130bf301b039e0d0ed2a
SpiralP <spiral.palmer@gmail.com>
2025-06-25 17:28:34 -0700

    build: play with workflow commands

--- .github/scripts/install-build-test.sh
+++ .github/scripts/install-build-test.sh
@@ -3,12 +3,21 @@ set -eux
 
 export CARGO_BUILD_TARGET="$1"
 
+echo '::group::setup rust'
 rustup default stable
-rustup component add rustfmt
+rustup component add clippy rustfmt
 rustup target add "$CARGO_BUILD_TARGET"
+echo '::endgroup::'
 
-cargo check --target "$CARGO_BUILD_TARGET"
+echo "::add-matcher::.github/matchers/rust.json"
+echo '::group::cargo clippy'
+cargo clippy --target "$CARGO_BUILD_TARGET"
+echo '::endgroup::'
 
-cargo build --target "$CARGO_BUILD_TARGET"
+# echo '::group::cargo build'
+# cargo build --target "$CARGO_BUILD_TARGET"
+# echo '::endgroup::'
 
+echo '::group::cargo test'
 cargo test --target "$CARGO_BUILD_TARGET" -- --nocapture --test-threads 1
+echo '::endgroup::'
--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,23 @@ async fn main() {
         .unwrap();
 
     println!("{body}");
+
+    // warning: unused variable: `unused_var`
+    //   --> src/main.rs:16:9
+    let unused_var = "unused";
+
+    // warning: statement with no effect
+    //   --> src/main.rs:20:5
+    ();
+    // warning: `github-reusable-workflows-example-rust` (bin "github-reusable-workflows-example-rust") generated 1 warning
+}
+
+#[test]
+fn test_ci_annotations() {
+    println!("");
+    println!("error[E0618]: expected function, found `&'static str`");
+    println!("  --> src/main.rs:16:22");
+    println!("");
+    println!("thread 'test_ci_annotations' panicked at src/main.rs:28:5:\nhehe");
+    println!("");
 }
