5fe177f1ad1666ab160454e9bd7f5c751bd8e4ed
SpiralP <spiral.palmer@gmail.com>
2025-06-26 00:37:23 -0700

    group

---
+++ .github/scripts/generate-annotations.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -eux
+
+OUTPUT_LOG_FILE="$1"
+
+# parse file and generate github annotations
--- .github/scripts/install-build-test.sh
+++ .github/scripts/install-build-test.sh
@@ -3,21 +3,22 @@ set -eux
 
 export CARGO_BUILD_TARGET="$1"
 
-echo '::group::setup rust'
-rustup default stable
-rustup component add clippy rustfmt
-rustup target add "$CARGO_BUILD_TARGET"
-echo '::endgroup::'
+group() {
+  echo "::group::$*"
+  set -x
+  "$@"
+  set +x
+  echo "::endgroup::"
+}
 
-echo "::add-matcher::.github/matchers/rust.json"
-echo '::group::cargo clippy'
-cargo clippy --target "$CARGO_BUILD_TARGET"
-echo '::endgroup::'
+setup_rust() {
+  rustup default stable
+  rustup component add clippy rustfmt
+  rustup target add "$CARGO_BUILD_TARGET"
+}
 
-# echo '::group::cargo build'
-# cargo build --target "$CARGO_BUILD_TARGET"
-# echo '::endgroup::'
+group setup_rust
 
-echo '::group::cargo test'
-cargo test --target "$CARGO_BUILD_TARGET" -- --nocapture --test-threads 1
-echo '::endgroup::'
+group cargo clippy --target "$CARGO_BUILD_TARGET"
+# group cargo build --target "$CARGO_BUILD_TARGET"
+group cargo test --target "$CARGO_BUILD_TARGET" -- --nocapture --test-threads 1
