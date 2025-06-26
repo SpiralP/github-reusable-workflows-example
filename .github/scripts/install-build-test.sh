e56e68dbc3267d17065084d1bcc5b7bc4ce165c0
SpiralP <spiral.palmer@gmail.com>
2025-06-25 17:28:34 -0700

    parse annotations with a script

--- .github/scripts/install-build-test.sh
+++ .github/scripts/install-build-test.sh
@@ -9,6 +9,8 @@ rustup target add "$CARGO_BUILD_TARGET"
 
 cargo check --target "$CARGO_BUILD_TARGET"
 
+cargo clippy --target "$CARGO_BUILD_TARGET"
+
 cargo build --target "$CARGO_BUILD_TARGET"
 
 cargo test --target "$CARGO_BUILD_TARGET" -- --nocapture --test-threads 1
